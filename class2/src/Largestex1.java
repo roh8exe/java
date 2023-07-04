bda6fd0f1730555bdb592820cd7e6916fc01ccf1
roh8exe <rohitbansal547@gmail.com>
2023-07-04 15:11:38 +0530

    123

--- class2/src/Largestex1.java
+++ class2/src/Largestex1.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 public class Largestex1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.print("enter first number: ");
                 int a = in.nextInt();
+        System.out.print("enter second number: ");
                 int b = in.nextInt();
+        System.out.print("enter third number: ");
                 int c =  in.nextInt();
 
                 int max = a;
