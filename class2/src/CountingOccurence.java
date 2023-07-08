ea94889b698f0ffd6c4ed11661b5f4c8b5002899
roh8exe <rohitbansal547@gmail.com>
2023-07-08 13:10:44 +0530

    123

--- class2/src/CountingOccurence.java
+++ class2/src/CountingOccurence.java
@@ -1,3 +1,18 @@
-public class CountingOccurence {
+import java.util.Scanner;
 
+public class CountingOccurence {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in) ;
+                int count =0 ;
+           int Fn = in.nextInt();
+           int Sn = in.nextInt();
+          while (Fn>0){
+       int rem = Fn % 10 ;
+       if (rem == Sn ) {
+       count++ ;
+   }
+    Fn  = Fn/10 ;
+}
+        System.out.println(count);
+}
 }
