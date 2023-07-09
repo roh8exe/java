4f858e3d5fc18de53d90aea976a89274ebb17729
roh8exe <rohitbansal547@gmail.com>
2023-07-09 11:42:14 +0530

    123

---
+++ 4.FunctionsOrMethods/src/Sum.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        sum();
+        sum();
+        sum();
+        sum();
+        sum();
+        sum();
+    }
+    static void sum() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter number 1: ");
+        int num1 = in.nextInt();
+        System.out.print("Enter number 2: ");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        System.out.println("The sum = " +sum);
+    }
+}
+
+
+/*
+      returm_typs name () {
+     //   body
+           return statement;
+       }
+ */
