4ad9ea58450e18976377eecac07702539e0465a9
roh8exe <rohitbansal547@gmail.com>
2023-07-11 12:04:49 +0530

    123

---
+++ 4.FunctionsOrMethods/src/Sum3.java
@@ -0,0 +1,11 @@
+public class Sum3 {
+    public static void main(String[] args) {
+        int ans = sum3(20,30);
+        System.out.println(ans);
+        }
+        //pass the value of numbers when you are calling the method in main()
+        static int sum3(int a , int b ){
+            int sum = a+b;
+            return sum;
+        }
+}
