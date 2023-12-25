9490094c679e69e47a00a68a0ab86d4b437db53d
roh8exe <rohitbansal547@gmail.com>
2023-12-25 11:46:53 +0530

    BinarySearch

---
+++ 0.Questions/BinarySearch/src/_4_FirstLastPosition.java
@@ -0,0 +1,44 @@
+public class _4_FirstLastPosition {
+    public static void main(String[] args) {
+        //sorted array
+
+    }
+
+    public int[] binarysearch(int[] nums, int target) {
+
+        int[] ans = {-1,-1};
+        //check for first occurrence if target firsts
+        int start = search(nums , target,true);
+        int end = search(nums , target,false);
+
+        ans[0] = start;
+        ans[1] = end;
+
+        return ans;
+    }
+    //this function just returns the index value of target
+    int search(int[] arr, int target, boolean findStartIndex) {
+        int ans = -1;
+        int start = 0;
+        int end = arr.length-1;
+
+        while (start<=end) {
+
+            int mid = start + (end - start) / 2 ;
+
+            if (target < arr[mid]) {
+                end = mid -1;
+            }else if (target > arr[mid]) {
+                start =  mid +1;
+            }else {
+                ans = mid;
+                if (findStartIndex) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
